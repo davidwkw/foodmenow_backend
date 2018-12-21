8200068ba2ec850b37b11763466ad956dea81d81
David <wangkhangwaei@hotmail.com>
2018-12-21 15:00:59 +0800

    first commit

--- foodmenow/views.py
+++ foodmenow/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
+# from food_me_now_backend.settings import YELP_SECRET_KEY
 import requests
 import json
+import os
+from food_me_now_backend.settings import YELP_SECRET_KEY
 
 # Create your views here.
 
 def bussiness_search(request):
+
     params = {'location' : 'New York City'}
     
-    r = requests.get('https://api.yelp.com/v3/businesses/search', params=params, headers={'Authorization': 'Bearer 53OigNCghYI1KMt3ESL-dBZRemhUA9QjxuNlu_nm8Mx7bkFJLrET_hqMYMKIG4RzQH8YXTwx1JRJWCqKiBXjZldqo8RoSAIO5_yUFWYFuqPenK8biBV4MSAcQ60YXHYx'})
-
-    print(type(r))
+    r = requests.get('https://api.yelp.com/v3/businesses/search', params=params, headers={'Authorization': f'Bearer {YELP_SECRET_KEY}'})
 
     data = r.json()
 
-    import pdb; pdb.set_trace()
-    print(type(data))
-    
-    return JsonResponse(r)
+    return JsonResponse(data)
