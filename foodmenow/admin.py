fec5b893e606bbc9c250ce693d7ae8b6087cca79
David <wangkhangwaei@hotmail.com>
2018-12-24 15:15:45 +0800

    allowed frontend and backend servers to run concurrently

--- foodmenow/admin.py
+++ foodmenow/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Users, Preferences
+from .models import User, Preference
 
 
 # Register your models here.
 
-admin.site.register(Users)
-admin.site.register(Preferences)
+admin.site.register(User)
+admin.site.register(Preference)
--- foodmenow/urls.py
+++ foodmenow/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     re_path(r'.{0,3}(latitude=(?P<latitude>-?\d+\.\d+)).{0,3}(longitude=(?P<longitude>-?\d+\.\d+))(.{0,3}radius=(?P<radius>\d+))?(.{0,3}price=(?P<price>\d))?(.{0,3}categories=(?P<categories>(\w+,?)+))?/$', views.restaurant_search),
-    path('<id>/reviews', views.restaurant_details),
-    path('<id>', views.restaurant_reviews),
+    path('<id>/reviews/', views.restaurant_details),
+    path('<id>/', views.restaurant_reviews),
     # path('create/', views.create_user),
     # path('login/', views.login_user)
 ]
