a888a6c85511486b258d099814b339b69dfea68e
David <wangkhangwaei@hotmail.com>
2018-12-28 18:38:53 +0800

    finished user preferences

--- foodmenow/urls.py
+++ foodmenow/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('businesses/<id>/', views.restaurant_reviews),
     path('user/create/', views.create_user),
     path('user/login/', views.login_user),
-    path('user/update/preferences/', views.update_preferences)
+    path('user/update/preferences/', views.update_preferences),
+    path('user/preferences/', views.user_preferences)
+    # path('uber/', views.uber_call)
 ]
 
 # re_path(r'search.{0,3}(latitude=(?P<latitude>-?\d+\.\d+)).{0,3}(longitude=(?P<longitude>-?\d+\.\d+))(.{0,3}radius=(?P<radius>\d+))?(.{0,3}price=(?P<price>\d))?(.{0,3}categories=(?P<categories>(\w+,?)+))?/?', views.restaurant_search),
